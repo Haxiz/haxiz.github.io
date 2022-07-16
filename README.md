518a0bb876b603cd51371dd0a8845a3f77afcac0
MrLar <23457286+MrLar@users.noreply.github.com>
2022-07-16 22:55:15 +0200

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# An Embed builder meant for use with Mantaro Bot
+
+This is an embed builder that is meant for users to be able to easily
+create embeds in a GUI. It will give them the format that [Mantaro](https://mantaro.site) expects of them
+
+# Credit
+This project is a heavily modified version of the Embed Visualizer made by Glitchii.\
+Source: https://github.com/Glitchii/embedbuilder \
+Site: https://glitchii.github.io/embedbuilder/ \
+License: https://github.com/Glitchii/embedbuilder\LICENSE
+
+The only files touched are the css, the index file, the builder config
+and the script. Everything else is unmodified and simply bundled with this repo
+to make it work in future days.
---
+++ builder.config.js
@@ -0,0 +1,26 @@
+/**
+ * This script file will (or atleast should) run before the main script file runs.
+ * This file should contain stuff like options, global variables (etc.) to be used by the main script.
+ * */
+
+// Options
+
+// URL options can override the options below.
+// Options set through the menu can override both.
+options = {
+    username: 'Mantaro',
+    avatar: 'https://cdn.discordapp.com/avatars/213466096718708737/84b83a87f8e7a1475f989cbbd76c48d8.webp?size=40',
+    verified: false,
+    noUser: false,
+    dataSpecified: false,
+    guiTabs: ['author', 'description'],
+    useJsonEditor: false,
+    reverseColumns: false,
+    allowPlaceholders: false,
+    autoUpdateURL: false,
+    hideEditor: false,
+    hidePreview: false,
+    hideMenu: false,
+    multiEmbeds: false,
+    noMultiEmbedsOption: true,
+}
